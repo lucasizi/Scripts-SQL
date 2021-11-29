3831e3c32bfa7cff84276500e238cc350e12e240
Lucas Izidoro <lucasizi10@hotmail.com>
2021-11-29 13:47:16 -0300

    Update py_to_plsql.sql

--- py_to_plsql.sql
+++ py_to_plsql.sql
@@ -5,7 +5,10 @@ SCRIPT COM INTUITO DE FAZER OS DESAFIOS QUE IMAGINO.
 */
 
 -- Desafio 1
--- Refazer o programa das casas em python para o PL/SQL (INCOMPLETO)
+-- Refazer o programa das casas em python para o PL/SQL (completo)
+-- O programa serve para fazer um calculo simples e descobrir quanto tempo você levara para comprar uma casa
+-- Ele pede o valor da casa em questão, seu salario e quantos porcento do seu salario vai querer economizar por mês.
+
 SET SERVEROUTPUT ON
 
 ACCEPT VCASA PROMPT 'INSIRA O VALOR DA CASA QUE DESEJA: '
@@ -33,13 +36,11 @@ BEGIN
     END LOOP;
 
 
-    vanos := (vconta/12);
-    vmeses := (vconta)-(vanos*12);
+    vanos := trunc(vconta/12);
+    vmeses := (vconta)-trunc(vanos*12);
     
-    dbms_output.put_line('vconta: '||vconta);
-    dbms_output.put_line('vanos: '||vanos);
-    dbms_output.put_line('vmeses: '||vmeses);
-    
+    dbms_output.put_line('--------------------------------------------------------');
+    dbms_output.put_line('Deste modo as parcelas ficam em '||vconta||'x de R$'||veconomia1 * vsalario1 / 100);
     if vconta = 1 THEN
         dbms_output.put_line('Voce levara '||vconta||' mes para comprar sua casa');
 
@@ -50,29 +51,22 @@ BEGIN
             dbms_output.put_line('Voce levara '||vanos||' ano para comprar sua casa');
 
         ELSIF vconta > 12 and vconta < 24 THEN
-            dbms_output.put_line('Voce levara '||vanos||' ano e '||vmeses|| 'meses para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vanos||' ano e '||vmeses||' meses para comprar sua casa');
 
         ELSIF vconta >= 24 and vmeses = 0 THEN
             dbms_output.put_line('Voce levara '||vanos||' anos para comprar sua casa');
 
         ELSIF vconta >= 24 and vmeses >= 1 THEN
-            dbms_output.put_line('Voce levara '||vanos||'anos e '||vmeses||' mese para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vanos||' anos e '||vmeses||' meses para comprar sua casa');
     END IF;
 
     if vbanco > vcasa1 THEN
-        dbms_output.put_line('E ainda sobram: '||(vbanco-vcasa1)||' da sua economia na ultima parcela');
+        dbms_output.put_line('E ainda sobram R$'||vbanco-vcasa1||' da sua economia na ultima parcela');
     END IF;
+    
+    dbms_output.put_line('--------------------------------------------------------');
 END;
 
-SELECT CAST(319/12 as numeric) FROM DUAL;
-
-/* Faltam poucas coisas para serem corrigidas agora
-    Tought; Passar o primeiro vconta dos prints para numero inteiro
-    talvez o cast(vonta) as integer, faça isso.
-    cast (vconta) as integer / 12
-    ou algo assim...
-    O proximo é mais complicado, transformar o resto da divisão em meses.
-*/
 
 -- criar "Gerador de desculpas", onde a pessoa entra com uma data e o programa gera uma desculpa aleatoria
 -- exemplo "no dia 25/05/2022 eu terei uma consulta médica" / "no dia 25/05/2022 eu terei uma reunião importante"
