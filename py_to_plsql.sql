8d6d5e8787561d8415272d05a0aa3e2ebc60d1f7
Lucas Izidoro <lucasizi10@hotmail.com>
2021-11-28 23:24:30 -0300

    Update py_to_plsql.sql

--- py_to_plsql.sql
+++ py_to_plsql.sql
@@ -14,12 +14,13 @@ ACCEPT VECONOMIA PROMPT 'QUANTOS PORCENTO QUER ECONOMIZAR POR MES: '
 
 DECLARE
 
-    vcasa1     INT;
+    vcasa1     FLOAT;
     vanos      FLOAT;
-    vsalario1  INT;
+    vsalario1  FLOAT;
     veconomia1 FLOAT;
-    vbanco     INT;
-    vconta     INT DEFAULT 1;
+    vbanco     FLOAT;
+    vconta     FLOAT DEFAULT 1;
+    vmeses     FLOAT;
 BEGIN
     vcasa1 := &vcasa;
     vsalario1 := &vsalario;
@@ -31,31 +32,39 @@ BEGIN
         VCONTA := VCONTA + 1;
     END LOOP;
 
-    vanos := (vconta/12);
 
+    vanos := (vconta/12);
+    vmeses := (vconta)-(vanos*12);
+    
+    dbms_output.put_line('vconta: '||vconta);
+    dbms_output.put_line('vanos: '||vanos);
+    dbms_output.put_line('vmeses: '||vmeses);
+    
     if vconta = 1 THEN
         dbms_output.put_line('Voce levara '||vconta||' mes para comprar sua casa');
 
         ELSIF vconta > 1 and vconta < 12 THEN
-            dbms_output.put_line('Voce levara '||vconta||'meses para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vconta||' meses para comprar sua casa');
 
         ELSIF vconta = 12 THEN
-            dbms_output.put_line('Voce levara '||vconta/12||'ano para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vanos||' ano para comprar sua casa');
 
         ELSIF vconta > 12 and vconta < 24 THEN
-            dbms_output.put_line('Voce levara '||(vconta/12)||'ano e '||(((vconta/12) - (vconta)) * 12)|| 'meses para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vanos||' ano e '||vmeses|| 'meses para comprar sua casa');
 
-        ELSIF vconta >= 24 and ROUND(vconta/12)= 0 THEN
-            dbms_output.put_line('Voce levara '||(vconta/12)||' anos para comprar sua casa');
+        ELSIF vconta >= 24 and vmeses = 0 THEN
+            dbms_output.put_line('Voce levara '||vanos||' anos para comprar sua casa');
 
-        ELSIF vconta >= 24 and ROUND(vconta/12) != 0 THEN
-            dbms_output.put_line('Voce levara '||(vconta/12)||'anos e '||(((vconta/12) - (vconta)) * 12)|| 'meses para comprar sua casa');
+        ELSIF vconta >= 24 and vmeses >= 1 THEN
+            dbms_output.put_line('Voce levara '||vanos||'anos e '||vmeses||' mese para comprar sua casa');
     END IF;
 
     if vbanco > vcasa1 THEN
-        dbms_output.put_line('E ainda sobram: '||(vbanco-vcasa1)|| 'da sua economia na ultima parcela');
+        dbms_output.put_line('E ainda sobram: '||(vbanco-vcasa1)||' da sua economia na ultima parcela');
     END IF;
 END;
+
+SELECT CAST(319/12 as numeric) FROM DUAL;
 
 /* Faltam poucas coisas para serem corrigidas agora
     Tought; Passar o primeiro vconta dos prints para numero inteiro
