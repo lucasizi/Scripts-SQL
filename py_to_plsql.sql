55cc3786ff6914597e1f7775d128bb7567f65d28
Lucas Izidoro <lucasizi10@hotmail.com>
2021-12-01 13:20:53 -0300

    Update py_to_plsql.sql

--- py_to_plsql.sql
+++ py_to_plsql.sql
@@ -6,12 +6,12 @@ SCRIPT COM INTUITO DE FAZER OS DESAFIOS QUE IMAGINO.
 
 -- Desafio 1
 -- Refazer o programa das casas em python para o PL/SQL (completo)
--- O programa serve para fazer um calculo simples e descobrir quanto tempo você levara para comprar uma casa
--- Ele pede o valor da casa em questão, seu salario e quantos porcento do seu salario vai querer economizar por mês.
+-- O programa serve para fazer um calculo simples e descobrir quanto tempo você levara para comprar uma casa/carro etc
+-- Ele pede o valor do item em questão, seu salario e quantos porcento do seu salario vai querer economizar por mês.
 
 SET SERVEROUTPUT ON
 
-ACCEPT VCASA PROMPT 'INSIRA O VALOR DA CASA QUE DESEJA: '
+ACCEPT VCASA PROMPT 'INSIRA O VALOR DO ITEM QUE DESEJA: '
 ACCEPT VSALARIO PROMPT 'DIGITE SEU SALARIO: '
 ACCEPT VECONOMIA PROMPT 'QUANTOS PORCENTO QUER ECONOMIZAR POR MES: '
 
@@ -40,28 +40,28 @@ BEGIN
     vmeses := (vconta)-trunc(vanos*12);
     
     dbms_output.put_line('--------------------------------------------------------');
+
     dbms_output.put_line('Deste modo as parcelas ficam em '||vconta||'x de R$'||veconomia1 * vsalario1 / 100);
+    
+    
     if vconta = 1 THEN
-        dbms_output.put_line('Voce levara '||vconta||' mes para comprar sua casa');
+        dbms_output.put_line('Voce levara '||vconta||' mes para finalizar sua compra');
 
         ELSIF vconta > 1 and vconta < 12 THEN
-            dbms_output.put_line('Voce levara '||vconta||' meses para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vconta||' meses para finalizar sua compra');
 
         ELSIF vconta = 12 THEN
-            dbms_output.put_line('Voce levara '||vanos||' ano para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vanos||' ano para finalizar sua compra');
 
         ELSIF vconta > 12 and vconta < 24 THEN
-            dbms_output.put_line('Voce levara '||vanos||' ano e '||vmeses||' meses para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vanos||' ano e '||vmeses||' meses finalizar sua compra');
 
         ELSIF vconta >= 24 and vmeses = 0 THEN
-            dbms_output.put_line('Voce levara '||vanos||' anos para comprar sua casa');
+            dbms_output.put_line('Voce levara '||vanos||' anos para finalizar sua compra');
 
         ELSIF vconta >= 24 and vmeses >= 1 THEN
-            dbms_output.put_line('Voce levara '||vanos||' anos e '||vmeses||' meses para comprar sua casa');
-    END IF;
-
-    if vbanco > vcasa1 THEN
-        dbms_output.put_line('E ainda sobram R$'||vbanco-vcasa1||' da sua economia na ultima parcela');
+            dbms_output.put_line('Voce levara '||vanos||' anos e '||vmeses||' meses para finalizar sua compra');
+            
     END IF;
     
     dbms_output.put_line('--------------------------------------------------------');
