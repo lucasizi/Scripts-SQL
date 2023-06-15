bd734b8e47202c9bcc2a02102befa93c5b947a12
Lucas Izidoro <lucasizi10@hotmail.com>
2023-06-15 17:06:21 -0300

    Update Packages.sql

--- Packages.sql
+++ Packages.sql
@@ -1,63 +1,120 @@
--- Criando especificacao do package
--- criando a package com 2 funções e uma procedure
-
+--criando especifica??o do package
+--criando a package com 2 fun??es e uma procedure
 CREATE OR REPLACE PACKAGE PKG_FUNC AS
+  
+    ---PROCEDURE SIMULA SALARIO AUMENTO
+    PROCEDURE PROC_SIMULA(n_func_id NUMBER);
+  -- get nome completo do funcionario
+    FUNCTION get_nomeCompleto(n_func_id NUMBER)
+      RETURN VARCHAR2;
+   --  get salario do funcionario
+    FUNCTION get_salario(n_func_id NUMBER)
+      RETURN NUMBER;
 
-    -- PROCEDURE SIMULA SALARIO AUMENTO
-    PROCEDURE PROC_SIMULA(N_FUNC_ID NUMBER);
-    -- GET NOME COMPLETO DO FUNCIONARIO
-    FUNCTION GET_NOMECOMPLETO(N_FUNC_ID NUMBER) RETURN VARCHAR2;
-    --GET SALARIO DO FUNCIONARIO
-    FUNCTION GET_SALARIO(N_FUNC_ID NUMBER) RETURN NUMBER;
-END PKG_FUNC;
+  END PKG_FUNC;
 
--- CRIANDO O BODY
--- PACKAGE PKG_FUNC BODY
-CREATE OR REPLACE PACKAGE BODY PKG_FUNC AS
--- GET FUNCIONARIO NOME COMPLETO
-FUNCTION GET_NOMECOMPLETO(N_FUNC_ID NUMBER) RETURN VARCHAR2 IS
-V_NOMECOMPLETO VARCHAR2(50);
-BEGIN
-    SELECT A.FIRST_NAME || ' , ' || A.LAST_NAME
-    INTO V_NOMECOMPLETO
-    FROM HR.EMPLOYEES A
-    WHERE A.EMPLOYEE_ID = N_FUNC_ID;
-    
-    RETURN V_NOMECOMPLETO;
-    
-    EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-    RETURN NULL;
-    WHEN TOO_MANY_ROWS THEN
-    RETURN NULL;
-    END;
-    -- END GET_NOMECOMPLETO
-    
-    -- GET SALARIO
-    FUNCTION GET_SALARIO(N_FUNC_ID NUMBER) RETURN NUMBER IS
+--criando o boby
+/*
+    Package PKG_FUNC body
+  */
+  CREATE OR REPLACE PACKAGE BODY PKG_FUNC AS
+  --get funcionario nomeCompleto
+    FUNCTION get_nomeCompleto(n_func_id NUMBER) RETURN VARCHAR2 IS
+        v_nomeCompleto VARCHAR2(50);
     BEGIN
-    SELECT A.SALARY
-    INTO N_SALARIO
-    FROM HR.EMPLOYEES A
-    WHERE A.EMPLOYEE_ID=N_FUNC_ID;
-    
-    RETURN N_SALARIO;
-    
+      SELECT a.FIRST_NAME || ',' ||  a.LAST_NAME
+      INTO v_nomeCompleto
+      FROM HR.EMPLOYEES a
+      WHERE a.employee_id = n_func_id;
+      
+      RETURN v_nomeCompleto;
+   
     EXCEPTION
     WHEN NO_DATA_FOUND THEN
-    RETURN NULL;
+      RETURN NULL;
     WHEN TOO_MANY_ROWS THEN
-    RETURN NULL;
+      RETURN NULL;
+    END; 
+    --end get_nomeCompleto
+   
+    -- get salario
+    FUNCTION get_salario(n_func_id NUMBER) RETURN NUMBER IS
+      n_salario NUMBER(8,2);
+    BEGIN
+      SELECT a.SALARY
+      INTO n_salario
+      FROM HR.EMPLOYEES a
+      WHERE a.EMPLOYEE_ID = n_func_id;
+   
+      RETURN n_salario;
+   
+      EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+          RETURN NULL;
+        WHEN TOO_MANY_ROWS THEN
+          RETURN NULL;
     END;
--- END GET_SALARIO
+-- END get_salario
+--PROCEDURE
 
--- PROCEDURE
-    PROCEDURE PROC_SIMULA(N_FUNC_ID NUMBER) IS
+  PROCEDURE PROC_SIMULA(n_func_id NUMBER) IS
     V_SAL_NOVO NUMBER(8,2);
     BEGIN
-        SELECT A.SALARY*1.10 INTO V_SAL_NOVO FROM HR.EMPLOYEES A
-        WHERE A.EMPLOYEE_ID=N_FUNC_ID;
-        DBMS_OUTPUT.PUT_LINE('O NOVO SALARIO É->> '||VAL_SAL_NOVO);
+        SELECT a.SALARY*1.10 INTO V_SAL_NOVO FROM HR.EMPLOYEES a
+        where a.EMPLOYEE_ID=n_func_id;
+        dbms_output.put_line('O Novo salario ?->> '||V_SAL_NOVO);
     END;
-    -- FIM PROCEDURE
-END PKG_FUNC;
+    --fim PROCEDURE
+  END PKG_FUNC;
+/
+
+--Chamada do package
+--package_name.package_element
+--Executando pkg
+
+SET SERVEROUTPUT ON SIZE 1000000;
+  DECLARE
+    n_salario NUMBER(8,2);
+    v_nome   VARCHAR2(50);
+    n_func_id NUMBER := &ID_FUNC;
+  BEGIN
+        v_nome    := PKG_FUNC.get_nomeCompleto(n_func_id);
+        n_salario := PKG_FUNC.get_salario(n_func_id);
+ 
+    IF v_nome  IS NOT NULL AND n_salario IS NOT NULL
+    THEN
+      dbms_output.put_line('Funcion?rio: ' || v_nome);
+      dbms_output.put_line('Recebe sal?rio = ' || n_salario);
+      PKG_FUNC.PROC_SIMULA(n_func_id);
+    END IF;
+  END;
+  
+--executando um objeto
+SET SERVEROUTPUT ON SIZE 1000000;
+  DECLARE
+    n_salario NUMBER(8,2);
+    v_nome   VARCHAR2(50);
+    n_func_id NUMBER := &empresa_id;
+  BEGIN
+   
+    v_nome   := PKG_FUNC.get_nomeCompleto(n_func_id);
+   
+    IF v_nome  IS NOT NULL 
+    THEN
+      
+      dbms_output.put_line('Funcion?rio: '|| v_nome);
+     
+    END IF;
+  END;
+
+
+--EXCUTANDO SO A PROCEDURE
+
+  SET SERVEROUTPUT ON SIZE 1000000;
+  DECLARE
+   
+    n_func_id NUMBER := &ID_FUNC;
+  BEGIN
+    PKG_FUNC.PROC_SIMULA(n_func_id);
+  END;
+  
